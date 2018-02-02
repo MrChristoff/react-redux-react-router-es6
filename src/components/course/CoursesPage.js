d6c98bb69a6f5da93d770a4add31740560391075
Lawrence <LAWRENCX@legal.regn.net>
2018-02-02 12:22:33 +0000

    removed jsx and functions from CoursesPage container component to begin SOC

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -11,27 +11,11 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            course: { title: "" }
-        };
-
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
     }
 
     /* Child functions
         - functions called by render()
     */
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-    onClickSave() {
-        // ReduxFlow 1 Calls the Action creator with the object
-        this.props.actions.createCourse(this.state.course);
-    }
     courseRow(course, index) {
         return <div key={index}>{course.title}</div>;        
     }
@@ -46,16 +30,6 @@ class CoursesPage extends React.Component {
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Courses</h2>
-                <input
-                   type="text"
-                   onChange={this.onTitleChange}
-                   value={this.state.course.title} />
-
-                <input
-                   type="submit"
-                   value="Save"
-                   onClick={this.onClickSave} />
             </div>
         );
     }
