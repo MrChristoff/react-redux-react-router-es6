b3574fffa685bb9ed3b24e6c3c4a4e79933026f6
Lawrence <LAWRENCX@legal.regn.net>
2018-02-02 14:44:28 +0000

    Added thunk API call

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,23 @@
 // Action Creator
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
 export function createCourse(course) {
    // ReduxFlow 2 after being called by dispatcher will pass current state and action to reducer
    return { type: types.CREATE_COURSE, course: course };
+}
+
+function loadCoursesSuccess(courses) {
+   return { type: types.LOAD_COURSES_SUCCESS, courses: courses }
+}
+
+// thunk dispatch - returns a function
+export function loadCourses(course) {
+   return function (dispatch) {
+      return courseApi.getAllCourses().then(courses => {
+         dispatch(loadCoursesSuccess(courses));
+      }).catch(error => {
+         throw(error);
+      });
+   }
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,17 +3,12 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
    switch (action.type) {
-      case types.CREATE_COURSE:
+      case types.LOAD_COURSES_SUCCESS:
       // ReduxFlow 3 after having the Action pass the state and action to perform
-      // (acording to the switch condition) it will take a copy of the original state (empty aray)
-      // and add the new object to the copied state, then return pass that new state to 
-      // the store and then mapStateToProps
-         return [...state,
-            Object.assign(
-               {},
-               action.course
-            )
-         ];
+      // (acording to the switch condition) it will take a copy of the original state,
+      // add the new object to the copied state, pass that new state to 
+      // the store and then mapStateToProps will be called.
+         return action.courses;
 
       default:
          return state;
